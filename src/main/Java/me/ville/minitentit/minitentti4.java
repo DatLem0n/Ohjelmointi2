de2f1ca03585b3e181152ac967d087ef5d8b7ef7
Ville Kujala <sahki232@gmail.com>
2023-02-19 16:06:03 +0200

    minitentti4

--- src/main/Java/me/ville/minitentit/minitentti4.java
+++ src/main/Java/me/ville/minitentit/minitentti4.java
@@ -3,6 +3,9 @@ package main.Java.me.ville.minitentit;
 public class minitentti4 {
     public static void main(String[] args) {
         System.out.println(almostEqual(1.2, 1.3, 0.15));
+        System.out.println(hasEqualColumnWidth("1,2 \n 3,4"));
+        System.out.println(factorial(5));
+        System.out.println(numOfCols("10,32456,2234"));
     }
     static boolean almostEqual(double a, double b, double c){
         if (a > b){
@@ -12,16 +15,30 @@ public class minitentti4 {
     }
     static boolean hasEqualColumnWidth(String s){
         String[] rows;
-        String[] columns;
         String[] column;
-        rows = (s.split("\\n"));
+        int columnlength = 0;
+        boolean equals = true;
+        rows = (s.split(System.lineSeparator()));
         for (int i = 0; i < rows.length; i++) {
             column = rows[i].split(",");
-            for (int j = 0; i < column.length; j++) {
-//TÄHÄN sit loppuun toi logiikka ku jaksaa
+            if(columnlength == 0) columnlength = column.length;
+            else {
+                if (columnlength != column.length) equals = false;
             }
         }
-        return true;
+        return equals;
     }
-
+    static int factorial(int n){
+        int result = 1;
+        for (int i = 1; i<=n; i++){
+            result = result*i;
+        }
+        return result;
+    }
+    static int numOfCols(String s){
+        int colAmount;
+        colAmount = s.split(",").length;
+        if (s.endsWith(",")) colAmount ++;
+        return colAmount;
+    }
 }
