4bfc34a9c37e505818b73afe8f09c6195b8632b2
Ville Kujala <sahki232@gmail.com>
2023-04-22 15:06:59 +0300

    kotitehtava3 final

--- src/main/Java/me/ville/kotitehtavat/kotitehtava4/Hangman.java
+++ src/main/Java/me/ville/kotitehtavat/kotitehtava4/Hangman.java
@@ -1,4 +1,42 @@
 package main.Java.me.ville.kotitehtavat.kotitehtava4;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hangman {
+
+    public boolean guess(Character c){
+        // The method compares the character entered as a parameter to the word being guessed and adds the guess
+        //to the list of guesses. If the character is found in the word being guessed, the method will return true. If
+        //character is not found from the word being guessed, the number of guesses is reduced by one and the
+        //method will return false.
+        return true;
+    }
+
+    public List<Character> guesses(){
+        //The method returns the guesses made (as a List of Character objects). Each character should be in the list
+        //only once (even if the user has guessed the same character several times).
+
+        List<Character> returnList = new ArrayList<>();
+
+        return returnList;
+    }
+
+    public int guessesLeft(){
+        // The method returns the number of remaining guesses. Note, must not be negative.
+        return 0;
+    }
+
+    public String word(){
+        // The method returns the selected word (unmasked, i.e., as read from the file)
+        return null;
+    }
+
+    public boolean theEnd() {
+        //The method indicates whether the game is over or not. The game ends if all the letters in the word are
+        //guessed correctly
+
+        return true;
+    }
+
 }
--- src/main/Java/me/ville/kotitehtavat/kotitehtava4/WordList.java
+++ src/main/Java/me/ville/kotitehtavat/kotitehtava4/WordList.java
@@ -1,4 +1,31 @@
 package main.Java.me.ville.kotitehtavat.kotitehtava4;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class WordList {
+
+    public List<String> giveWords()
+    {
+        //return words of wordlist in a list
+
+        List<String> returnList = new ArrayList<>();
+
+        return returnList;
+    }
+
+    public WordList theWordsOfLength(int length){
+        // The method returns a new WordList object with only the words whose length corresponds to the value of
+        // the variable given as a parameter
+        WordList WordList = new WordList();
+        return WordList;
+    }
+
+    public WordList theWordsWithCharacters(String someString){
+        // The method returns a new WordList object with only the words with the letters in the exact positions
+        // specified in the given string (and matching the length of that given string).
+        WordList WordList = new WordList();
+        return WordList;
+    }
 }
+
